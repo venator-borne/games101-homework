9d8bb61d5f718c2626321b8ab5c5f38fe4471295
Zhang Haoyu <haoyu.zhang@intel.com>
2024-11-09 19:03:14 +0800

    first commit

---
+++ Assignment1/┤·┬δ┐≥╝▄/test/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/haoyu-zhang/nv2/file/games101/Assignment1/┤·┬δ┐≥╝▄/test/test.cpp" "CMakeFiles/Demo.dir/test.cpp.o" "gcc" "CMakeFiles/Demo.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment1/┤·┬δ┐≥╝▄/test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char** argv) {
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << endl;
+    }
+    cout << argv[0] << endl;
+    
+}
